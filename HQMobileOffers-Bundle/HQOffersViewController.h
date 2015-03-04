3ab02fa92dcc682c8344fbf935718bcd6369ae0f
ynotski <smwcollins@yahoo.co.uk>
2015-03-04 19:01:56 +0700

    Added updated bundle

---
+++ HQMobileOffers-Bundle/HQOffersViewController.h
@@ -0,0 +1,41 @@
+//
+//  HQOffersViewController.h
+//  HQ_OFFERS
+//
+//  Created by Antony Collins on 04/03/2015.
+//  Copyright (c) 2015 Hotel Quickly. All rights reserved.
+//
+
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVCommandQueue.h>
+
+@interface HQOffersViewController : CDVViewController
+
+@end
+
+
+@interface MainCommandDelegate : CDVCommandDelegateImpl
+@end
+
+@interface MainCommandQueue : CDVCommandQueue
+@end
